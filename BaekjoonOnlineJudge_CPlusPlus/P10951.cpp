d8322cf19fdc78e5596dbcf34530551ffdc2c550
kms <k_rush@naver.com>
2022-01-05 12:55:33 +0900

    P10951(A+B-4) 문제 해결 - cin, cout 사용해보기

---
+++ BaekjoonOnlineJudge_CPlusPlus/P10951.cpp
@@ -0,0 +1,18 @@
+/** Baekjoon Online Judge
+*   Problem No. 10951
+*   A + B - 4
+*   Writed by Rush.K
+*   cin EOF
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+
+	while (cin >> a >> b) { // 테스트 케이스의 끝을 확인하며 연산
+		cout << a + b << "\n";
+	}
+}
